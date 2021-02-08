3cdbe2543aaa52929d6a8736c268394f17495d24
Capitan-Parrot <kondrandr2004@yandex.ru>
2021-02-08 18:01:44 +0400

    Формат карты

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import sys
 import pygame
 import requests
 from io import BytesIO
-
+import pygame_gui
 
 def terminate():
     pygame.quit()
     sys.exit()
 
 
-def get_image(pos, zoom):
+def get_image(pos, zoom, format=0):
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     map_params = {
         'll': pos,
         'z': zoom,
-        'l': 'map'
+        'l': ['map', 'sat', 'sat,skl'][format]
     }
     response = requests.get(map_api_server, params=map_params)
     if not response:
@@ -30,24 +30,36 @@ def get_image(pos, zoom):
 zoom = input()
 # Координаты (разделяются запятой)
 coords = input()
-
+width, height = 600, 450
 # Инициализация
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("MapsAPI")
 clock = pygame.time.Clock()
 FPS = 60
+manager = pygame_gui.UIManager((width, height))
 
 running = True
 # 4
 # 29.897824,59.865449
-
+type_map = 0
 screen.blit(pygame.image.load(BytesIO(get_image(coords, zoom))), (0, 0))
+rules = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((width - 105, height - 90), (100, 50)),
+                                     text='Вид карты',
+                                     manager=manager)
 while running:
+    timedelta = clock.tick(FPS) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element.text == 'Вид карты':
+                    type_map = (type_map + 1) % 3
+                    screen.blit(pygame.image.load(BytesIO(get_image(coords, zoom, type_map))), (0, 0))
+        manager.process_events(event)
+    manager.update(timedelta)
+    manager.draw_ui(screen)
     pygame.display.flip()
     clock.tick(FPS)
 
