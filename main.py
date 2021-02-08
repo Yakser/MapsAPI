aa2b57f1084f60bbbc4304afb9d16a8ac1a95ae2
Sergey Yaksanov <iaksanov2044@mail.ru>
2021-02-08 17:08:06 +0400

    Шаблон окна 2

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import os
 import sys
 import pygame
 import requests
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("MapsAPI")
+clock = pygame.time.Clock()
+FPS = 60
 # Масштаб карты
 zoom = float(input())
 # Координаты (разделяются запятой)
@@ -12,13 +21,15 @@ coords = input()
 
 running = True
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
+
     pygame.display.flip()
-pygame.quit()
+    clock.tick(FPS)
+
+terminate()
 
 # main
 # # Created by Sergey Yaksanov at 08.02.2021
