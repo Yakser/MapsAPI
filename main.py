320b51251456b3e5a481219cdf45f06ce8a5eca2
Sergey Yaksanov <iaksanov2044@mail.ru>
2021-02-08 17:18:22 +0400

    Получение изображения карты

--- main.py
+++ main.py
@@ -9,13 +9,35 @@ def terminate():
     sys.exit()
 
 
+def get_image(pos, zoom):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    map_params = {
+        'll': pos,
+        'z': zoom,
+        'l': 'map'
+    }
+    response = requests.get(map_api_server, params=map_params)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(response.content)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+# Инициализация
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("MapsAPI")
 clock = pygame.time.Clock()
 FPS = 60
+
 # Масштаб карты
-zoom = float(input())
+zoom = input()
 # Координаты (разделяются запятой)
 coords = input()
 
