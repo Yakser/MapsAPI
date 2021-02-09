2fc9e475a2d50bfa8666976853e3bac41de6b267
Sergey Yaksanov <iaksanov2044@mail.ru>
2021-02-09 22:08:18 +0400

    bugfix - Ввод некорректного запроса

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import os
 import sys
 import pygame
 import requests
 from io import BytesIO
 import pygame_gui
+
 
 def terminate():
     pygame.quit()
@@ -48,8 +48,8 @@ running = True
 type_map = 0
 screen.blit(pygame.image.load(BytesIO(get_image(coords, zoom))), (0, 0))
 view = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((width - 115, height - 90), (100, 50)),
-                                     text='Вид карты',
-                                     manager=manager)
+                                    text='Вид карты',
+                                    manager=manager)
 name_obj = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((width // 2 - 140, height - 50), (250, 200)),
                                                manager=manager)
 search = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((width // 2 + 115, height - 50), (55, 30)),
@@ -104,6 +104,7 @@ while running:
                 if event.ui_element.text == 'Вид карты':
                     type_map = (type_map + 1) % 3
                     screen.blit(pygame.image.load(BytesIO(get_image(coords, zoom))), (0, 0))
+
                 if event.ui_element.text == 'Искать':
                     geocoder = "http://geocode-maps.yandex.ru/1.x/"
                     name = name_obj.get_text()
@@ -114,11 +115,14 @@ while running:
 
                     response = requests.get(geocoder, params=geocoder_params)
                     json_response = response.json()
-                    toponym = json_response["response"]["GeoObjectCollection"][
-                        "featureMember"][0]["GeoObject"]
-                    coords = list(map(float, toponym["Point"]["pos"].split()))
-                    pt = ','.join(map(str, coords))
-                    screen.blit(pygame.image.load(BytesIO(get_image(coords, zoom))), (0, 0))
+                    if json_response['response']['GeoObjectCollection']['metaDataProperty'][
+                            'GeocoderResponseMetaData']['found'] != '0':
+                        toponym = json_response["response"]["GeoObjectCollection"][
+                            "featureMember"][0]["GeoObject"]
+                        coords = list(map(float, toponym["Point"]["pos"].split()))
+                        pt = ','.join(map(str, coords))
+                        screen.blit(pygame.image.load(BytesIO(get_image(coords, zoom))), (0, 0))
+
         manager.process_events(event)
     manager.update(timedelta)
     manager.draw_ui(screen)
@@ -129,4 +133,4 @@ terminate()
 
 # main
 # # Created by Sergey Yaksanov at 08.02.2021
-# Copyright © 2020 Yakser. All rights reserved.
+# Copyright © 2020 Yakser. All rights reserved
